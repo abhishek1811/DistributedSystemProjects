38b6ac9a88b3389c35a7c3842e2e7332b72e3a18
abhishek1811 <Abhishek@Abhisheks-MacBook-Pro.local>
2015-01-11 14:26:46 -0500

    Indentation fixed

--- Bitcoin Miner/Server/src/main/scala/Server.scala
+++ Bitcoin Miner/Server/src/main/scala/Server.scala
@@ -37,11 +37,11 @@ case class Work (from : Int , end : Int , leadingZeroes : Int) extends BitcoinMe
    * Checks whether whether the command line argument is valid or not.
    * Also as per defined condition sets the value for numberOfZeros.
    */
-   def assign(argument:String)={
+  def assign(argument:String)={
     //If argument is empty, then exit with error message .
-    if(argument == " ")
-    {
-      println("*********Invalid Argument**********" +"\n Input should be of type : \n scala Project1 leadingZeroes   or\n scala Project1 IPAddress" +"\n ***********************************")
+    if(argument == " "){
+      println("*********Invalid Argument**********" +"\n Input should be of type :") 
+      println("\n scala Project1 leadingZeroes or\n scala Project1 IPAddress" +"\n ***********************************")
       System.exit(1)
     }
     //If it contains " . " which depicts it might be IP address.Thus assign numberofZeroes........ Error not handles " ."
@@ -61,12 +61,12 @@ case class Work (from : Int , end : Int , leadingZeroes : Int) extends BitcoinMe
   val master = system.actorOf(Props(new Master(s_actors,bitcoinRange,numberOfZeros)),name = "master") 
   println(master.path)
   master ! FindBitcoin
-  
+
   /*
    *  Worker class is used to do all the mining of bitcoins when master gives the work. Actors of this  
    *  class are defined in Master class. Each worker actor is assigned a chunk to mine required Bitcoins.
    */
-   class Worker extends Actor {
+  class Worker extends Actor {
 
     // generates pattern according to numberOfZeros and returns the pattern created.
     def createPattern(max:Int)={  
@@ -82,8 +82,7 @@ case class Work (from : Int , end : Int , leadingZeroes : Int) extends BitcoinMe
      *  each string formed in the chunk and checks whether it is required Bitcoin or not. Updates number
      *  of bitcoin mined in coincounter variable. Return message Done which is defined in Master's receive.
      */
-     def shaHasher(from: Int, end: Int, numberOfZeros: Int) =
-     {  
+    def shaHasher(from: Int, end: Int, numberOfZeros: Int) ={  
       var pattern: String = createPattern(numberOfZeros)
       var hashstrings: String = ""
       // instantiates the object of sha-256 to sha
@@ -94,7 +93,7 @@ case class Work (from : Int , end : Int , leadingZeroes : Int) extends BitcoinMe
 	      var hexstring = sha.digest(("n.sadhwani" + i.toString()).getBytes).foldLeft("")((s: String, b: Byte) => s +
          Character.forDigit((b & 0xf0) >> 4, 16) + Character.forDigit(b & 0x0f, 16))
 	      // select all the hash strings which starts with required pattern and update coincounter as it.
-	      if(hexstring.startsWith(pattern)) {
+	      if(hexstring.startsWith(pattern)){
           hashstrings += "n.sadhwani" + i + " " + hexstring + "\n"
           coincounter +=1
           println(hashstrings)
@@ -107,19 +106,18 @@ case class Work (from : Int , end : Int , leadingZeroes : Int) extends BitcoinMe
        * Message that accepts assigned chunk to for each worker and reply sender with information of number of chunks
        * processed and Bitcoins mined through function shaHasher.
        */
-       case Work(from,end,leadingZeroes) ⇒
-       sender ! shaHasher(from,end,leadingZeroes)
-     }
-
-   }
+      case Work(from,end,leadingZeroes) ⇒
+        sender ! shaHasher(from,end,leadingZeroes)
+    }
+  }
 
   /*
    *  Master actor assign chunks to its worker and remote client till all chunks to be processed are over.
    *  If remote client contacts the master for work then master assign some chunks to client otherwise master
    *  assigns all chunks to its worker.
    */
-   class Master(noOfWorkers:Int, TotalElements:Int, leadingZeroes:Int) extends Actor 
-   {
+  class Master(noOfWorkers:Int, TotalElements:Int, leadingZeroes:Int) extends Actor {
+    
     // client variables used to define chunk for clients and also bitcoins processed by client.
     var clientCount = 1
     var clientBitcoins = 0 
@@ -136,47 +134,44 @@ case class Work (from : Int , end : Int , leadingZeroes : Int) extends BitcoinMe
     def receive = {
     	// sends information respective clients.
       case "Ready" => 	
-      println ("I am sending")
-       //sender ! getWork(clientworker,clientChunkFrom,clientChunkEnd,leadingZeroes)
-       sender !  clientChunkFrom
-       sender !  clientChunkEnd
-       sender !  leadingZeroes
-       sender !  clientworker 
-       clientChunkFrom = clientChunkFrom + clientBuffer
-       clientChunkEnd = clientChunkEnd + clientBuffer
+        println ("I am sending")
+        //sender ! getWork(clientworker,clientChunkFrom,clientChunkEnd,leadingZeroes)
+        sender !  clientChunkFrom
+        sender !  clientChunkEnd
+        sender !  leadingZeroes
+        sender !  clientworker 
+        clientChunkFrom = clientChunkFrom + clientBuffer
+        clientChunkEnd = clientChunkEnd + clientBuffer
       /* 
        * using RoundRobin Algorithm sending chunks of size 10000 to master's worker till all chunks
        * are processed
        */
-       case FindBitcoin =>
-       for (i <-1 until numberOfChunks)
-       {
-         workerRouter ! Work((i-1) * 10000, i * 10000, numberOfZeros)	
-       }
+      case FindBitcoin =>
+        for (i <-1 until numberOfChunks){
+          workerRouter ! Work((i-1) * 10000, i * 10000, numberOfZeros)	
+        }
 	    /*
        * Checks if all chunks are processed or not. Once all chunks are processed it prints
        * the total number of bitcoins processed by Server and Client.At end terminates the 
        * server with system shutdown.
        */ 
-       case Done(shaDone) => 
-       chunkCounter += 1
-       if(chunkCounter == numberOfChunks){
+      case Done(shaDone) => 
+        chunkCounter += 1
+        if(chunkCounter == numberOfChunks){
          println("Total Bit Coins Mined :" + (shaDone+clientBitcoins) + "\n Terminating..........")
          context.system.shutdown()
   		    //context.stop(self)		
-      }
+        }
       // print the strings accepted from client
       case msg: String => 
-      println("*****---------Bitcoins received from client "+clientCount+"-----------------------*********")
-      clientCount = clientCount + 1
-      println(msg.toString) 	
-      println("*********------------------------------------------------------------------*****************")
+        println("*****---------Bitcoins received from client "+clientCount+"-----------------------*********")
+        clientCount = clientCount + 1
+        println(msg.toString) 	
+        println("*********------------------------------------------------------------------*****************")
       // prints the number of Bitcoins processed by Client.
       case noofbitcoins:Int=>  
-      clientBitcoins+=noofbitcoins
-      println("\n\n No of Bitcoins mined by clients is :"+noofbitcoins)      
+        clientBitcoins+=noofbitcoins
+        println("\n\n No of Bitcoins mined by clients is :"+noofbitcoins)      
     }
-
   }
-
 }
